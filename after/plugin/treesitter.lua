374508be479badec1147f5f04751a677183c0aa4
TeleVoyant <ditielo96.dtl@gmail.com>
2023-10-16 23:27:24 +0300

    License and Readme

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -27,7 +27,7 @@ require'nvim-treesitter.configs'.setup {
         -- list of language that will be disabled
 
         -- COMMENTED FROM HERE
-        --disable = { "c", "rust" },
+        disable = { "*.md" },
         ---- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         --disable = function(lang, buf)
         --    local max_filesize = 100 * 1024 -- 100 KB
